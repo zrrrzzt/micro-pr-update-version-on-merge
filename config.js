20fa7418817791b10f8b66e7920603df4705ecd7
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-21 07:30:52 +0200

    Implements signature-validation

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 'use strict'
 
 module.exports = {
+  GITHUB_WEBHOOK_SECRET: process.env.GITHUB_WEBHOOK_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   GITHUB_TOKEN: process.env.GITHUB_TOKEN || 'GITHUB_TOKEN',
   GITHUB_USER: process.env.GITHUB_USER || 'GITHUB_USER'
 }
--- index.js
+++ index.js
@@ -6,19 +6,33 @@ const { json, send } = require('micro')
 const repackData = require('./lib/repack-data')
 const getPkg = require('./lib/get-pkg')
 const updateVersion = require('./lib/update-version')
+const validatePayload = require('./lib/validate-payload')
 
 module.exports = async (request, response) => {
   if (request.method === 'POST') {
-    let data = repackData(await json(request))
-    const pkg = await getPkg(data.pkgUrlRaw)
-    data.pkg = pkg
+    const headers = request.headers
+    const payload = await json(request)
+    const isValid = validatePayload({headers: headers, body: payload})
 
-    const updates = await updateVersion(data)
-    const result = {
-      action: updates.action,
-      data: data
+    if (isValid === true) {
+      try {
+        let data = repackData(payload)
+        const pkg = await getPkg(data.pkgUrlRaw)
+        data.pkg = pkg
+
+        const updates = await updateVersion(data)
+        const result = {
+          action: updates.action,
+          data: data
+        }
+        send(response, 200, result)
+      } catch (error) {
+        console.error(error)
+        send(response, 500, {success: false, error: error.message})
+      }
+    } else {
+      send(response, 401, 'invalid signature')
     }
-    send(response, 200, result)
   } else {
     const readme = readFileSync('./README.md', 'utf-8')
     const html = marked(readme)
